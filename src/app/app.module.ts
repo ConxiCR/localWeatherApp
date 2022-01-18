59173965dd7e66295cb498c0f7f98680fd959fd3
ConxiCr <conxicr@hotmail.com>
2022-01-18 09:19:57 +0100

    creating local weather, retrieve current weather
    Signed-off-by: ConxiCr <conxicr@hotmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrentWeatherComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/current-weather/current-weather.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ICurrentWeather } from '../interface';
+
+@Component({
+  selector: 'app-current-weather',
+  templateUrl: './current-weather.component.html',
+  styleUrls: ['./current-weather.component.css']
+})
+export class CurrentWeatherComponent implements OnInit {
+
+  current: ICurrentWeather
+
+  constructor() { 
+    this.current = {
+      city:'Bethesda',
+      country: 'US',
+      date: new Date(),
+      image: 'assets/img/cold.png',
+      description: 'Freezen',
+    } as ICurrentWeather
+  }
+
+  ngOnInit(): void {
+  }
+
+}
