961c21e88a61fc118b57610c0ffefaa664bf0f66
ConxiCr <conxicr@hotmail.com>
2022-01-26 22:01:11 +0100

    new interface because OpenWeather API needs more information
    Signed-off-by: ConxiCr <conxicr@hotmail.com>

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,9 +1,25 @@
 import { HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+interface ICurrentWeatherData{
+  weather: [{
+    description: string,
+    icon: string
+  }],
+  main: {
+    temp: number
+  },
+  sys: {
+    country: string
+  },
+  dt: number,
+  name: string
+}
+
 @Injectable({
   providedIn: 'root' 
 })
+
 export class WeatherService {
 
   constructor(private httClient: HttpClientModule) { }
